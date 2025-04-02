dc186f7bfcfa6dd6b46a02203a64d5df08de3d45
Samuel <colaco.samuel8@gmail.com>
2025-04-02 09:27:53 -0300

    Update index in RefundController

--- src/Controllers/RefundsController.ts
+++ src/Controllers/RefundsController.ts
@@ -15,9 +15,40 @@ interface RequestUpdateBody{
 export class RefundsControllers{
 
     async index(req: Request, res: Response){
-        const refunds = await prisma.refunds.findMany()
 
-        res.status(200).json({ "message": refunds.length > 0 ? refunds : "Não tem reembolsos"})
+        const querySchema = z.object({
+            name: z.string().optional().default(""),
+            page: z.coerce.number().optional().default(1),
+            perPage: z.coerce.number().optional().default(10)
+        })
+
+        const { name, page, perPage } = querySchema.parse(req.query)
+
+        const skip = (page - 1) * perPage
+
+        const refunds = await prisma.refunds.findMany({ skip, take: perPage, 
+            where: { 
+                user: { 
+                    name: {
+                            contains: name.trim()
+                        }
+                }
+            }
+        })
+
+        const totalRecords = await prisma.refunds.count({ 
+            where: { 
+                user: { 
+                    name: {
+                            contains: name.trim()
+                    }
+                }
+            }
+        })
+
+        const totalPages = Math.ceil(totalRecords/ perPage)
+
+        res.status(200).json({ "message": refunds.length > 0 ? refunds : "Não tem reembolsos", "pagination": page, perPage, totalRecords, totalPages: totalPages > 0 ? totalPages : 1 })
     }
 
     async indexById(req: Request, res: Response){
