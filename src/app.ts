02c7a76554e0fd52cf2b0023c415e84941cb09d2
Samuel <colaco.samuel8@gmail.com>
2025-04-05 13:33:16 -0300

    Create routes for show image

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,14 @@ import { userRoutes } from "./routes/userRoutes"
 import { sessionRoutes } from "./routes/sessionRoutes"
 import { refundRoutes } from "./routes/refundRoutes"
 import { uploadRoutes } from "./routes/uploadRoutes"
+import upload from "./config/upload"
 
 export const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/uploads", express.static(upload.UPLOADS_FOLDER))
 
 app.use(userRoutes)
 app.use(sessionRoutes)
